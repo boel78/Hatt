29a1e9f5dfdb675acd0e4542d3777c81a48544ed
lukasdenfete <153369307+lukasdenfete@users.noreply.github.com>
2024-04-03 16:17:59 +0200

    börjat med databasklass

---
+++ src/hatt/Database.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hatt;
+
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+/**
+ *
+ * @author lukasdenfete
+ */
+public class Database {
+    private InfDB idb;
+    
+    public Database(){
+        try {
+            idb = new InfDB("hattmakardb", "3306", "hattmakare", "Hattsweatshop");
+        }
+        catch(InfException ex){
+            ex.printStackTrace();
+        }
+    }
+   
+
+}
