2dd96d3e5b05ad2a70d6ef62e2f300e6d7e2ac18
boel78 <95147718+boel78@users.noreply.github.com>
2024-04-03 15:25:22 +0200

    Fixade test klass

---
+++ src/hatt/Test.java
@@ -0,0 +1,29 @@
+package hatt;
+
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+public class Test {
+    
+    private static InfDB idb;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            idb = new InfDB("hattmakardb", "3306", "hattmakare", "Hattsweatshop");
+        }
+        catch(InfException ex){
+            ex.printStackTrace();
+        }
+        try{
+        System.out.println(idb.fetchRow("SELECT * FROM Kund"));
+        System.out.println("Test lyckades!");
+        }
+        catch(InfException ex){
+            ex.printStackTrace();
+        }
+    }
+    
+}
--- src/hatt/main.java
+++ src/hatt/main.java
@@ -3,29 +3,14 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package hatt;
-import oru.inf.InfDB;
-import oru.inf.InfException;
 
 public class main {
     
-    private static InfDB idb;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        try {
-            idb = new InfDB("hattmakardb", "3306", "hattmakare", "Hattsweatshop");
-        }
-        catch(InfException ex){
-            ex.printStackTrace();
-        }
-        try{
-        System.out.println(idb.fetchRow("SELECT * FROM Kund"));
-        }
-        catch(InfException ex){
-            ex.printStackTrace();
-        }
+        
     }
-    
 }
