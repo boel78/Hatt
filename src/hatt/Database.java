17e60f489e8fb6ea8e8dac4acdeeb14c72b18158
boel78 <95147718+boel78@users.noreply.github.com>
2024-04-05 14:44:03 +0200

    uppdaterar datbas

--- src/hatt/Database.java
+++ src/hatt/Database.java
@@ -7,53 +7,61 @@ package hatt;
 import java.util.ArrayList;
 import oru.inf.InfDB;
 import oru.inf.InfException;
+import java.util.HashMap;
 
 /**
  *
  * @author lukasdenfete
  */
 public class Database {
+
     private static InfDB idb;
-    
-    public Database(){
+
+    public Database() {
         try {
             idb = new InfDB("hattmakardb", "3306", "hattmakare", "Hattsweatshop");
-        }
-        catch(InfException ex){
+        } catch (InfException ex) {
             ex.printStackTrace();
         }
     }
-   
-    public static ArrayList<String> getAllCustomerID(){
-        ArrayList<String> customerIDList = new ArrayList<>(); 
-        
-        try
-        {
+
+    public static ArrayList<String> getAllCustomerID() {
+        ArrayList<String> customerIDList = new ArrayList<>();
+
+        try {
             customerIDList = idb.fetchColumn("SELECT cid FROM customer");
-        }
-        catch (InfException ex)
-        {
+        } catch (InfException ex) {
             ex.printStackTrace();
         }
-        
+
         return customerIDList;
     }
-    
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         new Database();
     }
-    
-      public String fetchSingle(String columnName, String tableName, String columnWhere, String columnIdentifier){
+
+    public String fetchSingle(String columnName, String tableName, String columnWhere, String columnIdentifier) {
         String query = "SELECT " + columnName + " FROM " + tableName + " WHERE " + columnWhere + " = '" + columnIdentifier + "'";
         String response = "";
-        try{
+        try {
             response = idb.fetchSingle(query);
-        }
-        catch(InfException ex){
-            System.out.println(ex.getMessage());
-            
+        } catch (InfException ex) {
+            ex.printStackTrace();
+
         }
         return response;
     }
-    
+
+    public HashMap<String, String> fetchRow(String tableName, String where, String whereIdentifier) {
+        String query = "SELECT * FROM" + tableName + " WHERE " + where + " = '" + whereIdentifier + "'";
+        HashMap<String, String> response = new HashMap<String, String>();
+        try {
+            response = idb.fetchRow(query);
+        } catch (InfException ex) {
+            ex.printStackTrace();
+        }
+        return response;
+    }
+
 }
