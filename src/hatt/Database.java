e8cffbe5be173034a27c0ba38b43e95317c1a456
abornerud <alexander.bornerud@gmail.com>
2024-04-08 10:30:26 +0200

    corection

--- src/hatt/Database.java
+++ src/hatt/Database.java
@@ -58,7 +58,7 @@ public class Database {
     }
       
         public static HashMap<String, String> fetchRow(String tableName, String where, String whereIdentifier) {
-        String query = "SELECT * FROM " + tableName + " WHERE " + where + " = '" + whereIdentifier + "';";
+        String query = "SELECT * FROM " + tableName + " WHERE " + where + " = " + whereIdentifier + ";";
         HashMap<String, String> response = new HashMap<String, String>();
         System.out.println(query);
         try {
