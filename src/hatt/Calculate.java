ea23d20f7de18925b558c25cf74aabec401fe0e5
boel78 <95147718+boel78@users.noreply.github.com>
2024-04-16 11:11:40 +0200

    skapade klass

---
+++ src/hatt/Calculate.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package hatt;
+
+/**
+ *
+ * @author joakimfockstedt
+ */
+public class Calculate {
+    
+    public Calculate(){
+    
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+    
+    
+    
+    
+}
