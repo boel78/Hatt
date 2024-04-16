d02fc6b6144c5dc0b5b1518980c36a091be603e9
boel78 <95147718+boel78@users.noreply.github.com>
2024-04-16 14:32:53 +0200

    fdxfdf

--- src/hatt/CreateOrderExistingCustomer.java
+++ src/hatt/CreateOrderExistingCustomer.java
@@ -252,7 +252,7 @@ public class CreateOrderExistingCustomer extends javax.swing.JFrame {
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(lblPrice)
                             .addComponent(txtEstimatedPrice, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(116, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
